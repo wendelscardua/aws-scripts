1749ddb546aaf448002a3e4fbdf472fe7ef41f69
Wendel Scardua <wendelscardua@gmail.com>
2021-05-29 14:37:28 -0300

    Add aws-mfa.rb (script for temporary MFA credentials)

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # aws-scripts
-Some helpful scripts for AWS stuff
+Some helpful ruby scripts for AWS stuff
+
+## aws-mfa.rb
+
+Adds (or updates) temporary mfa credentials to your `.aws/credentials` file.
+
+Usage:
+
+```sh
+$ ruby aws-mfa.rb
+```
+
+This will use your current credentials (usually from  default profile, but you can change
+this via AWS_PROFILE environment variable) to fetch your current user id, aws account and
+arn. These will be shown for confirmation.
+
+Then, you'll be asked for the serial number of your MFA. By default, it'll assume it's a
+virtual one, named after your arn (for example, if the user arn is
+`arn:aws:iam::1234567890:user/username` the default mfa arn will be
+`arn:aws:iam::1234567890:mfa/username`).
+
+After that you'll be asked for the duration (in seconds) for these credentials. The minimum
+value is 900 (= 15 minutes).
+
+Finally you'll be asked for the MFA code itself.
+
+The script will parse or create an `.aws/credentials` file in your home directory, then it'll add
+(or update) an `[mfa]` section with the temporary credentials to it... and that's it. Now you can
+simply use the `mfa` profile when using AWS CLI and other scripts.
---
+++ aws-mfa.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+
+  gem 'aws-sdk-sts'
+  gem 'dotini'
+  gem 'pry'
+end
+
+sts_client = Aws::STS::Client.new
+
+identity = sts_client.get_caller_identity
+
+puts "user_id: #{identity.user_id}"
+puts "account: #{identity.account}"
+puts "arn: #{identity.arn}"
+
+mfa_arn = identity.arn.gsub(%r{:user/}, ':mfa/')
+puts "assuming mfa arn is: #{mfa_arn}, is that correct? (Y/n) "
+
+prompt = nil
+
+loop do
+  prompt = gets.chomp
+  break if prompt =~ /\A[YyNn]?\z/
+end
+
+if prompt =~ /\A[Nn]\z/
+  puts 'mfa arn: '
+  mfa_arn = gets.chomp
+end
+
+puts 'Desired duration for session (in seconds) :'
+duration = gets.to_i
+
+puts 'MFA code: '
+token_code = gets.chomp
+
+response = sts_client.get_session_token(duration_seconds: duration, serial_number: mfa_arn, token_code: token_code)
+
+credentials_file = File.join(Dir.home, '.aws', 'credentials')
+
+credentials_ini = if File.exist?(credentials_file)
+                    Dotini::IniFile.load(credentials_file)
+                  else
+                    Dotini::IniFile.new
+                  end
+
+credentials_ini['mfa']['aws_access_key_id'] = response.credentials.access_key_id
+credentials_ini['mfa']['aws_secret_access_key'] = response.credentials.secret_access_key
+credentials_ini['mfa']['aws_session_token'] = response.credentials.session_token
+credentials_ini['mfa']['expiration'] = response.credentials.expiration
+
+File.open(credentials_file, 'wb') do |file|
+  credentials_ini.write(file)
+end
